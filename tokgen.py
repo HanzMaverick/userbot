b3f28b24da1fc5636010e1894952a0585e049047
Hanz Maverick <maverick_ruiz@mesoamericana.edu.gt>
2025-05-11 13:35:37 -0600

    fixed session

--- tokgen.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-import os
-
-API_ID = 21355756
-API_HASH = "91e2d7d11800a359759128e9f8994b0c"
-
-with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-    print("STRING_SESSION:", client.session.save())
